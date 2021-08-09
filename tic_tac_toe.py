d8b94da0c2112117212cab4b5ffa8fc4e2f54897
EugeneNdungu <mwangieugene80@gmail.com>
2021-08-09 09:31:16 +0300

    prompts user for input

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -15,4 +15,25 @@ def displayBoard():
             continue
         print('-'*9)
 
-displayBoard()
+displayBoard()  
+for i in range(3):
+    player1 = str(input('Player 1. Do you want to be X or O?\n'))
+    if player1 == 'X':
+        player2 = 'O'
+        break
+    elif player1 == 'O':
+        player2 = 'X'
+        break
+    else:
+        if i == 2:
+            print("Sorry. Out of trys. Better luck next time")
+            break
+        print('Invalid input! Try again')
+
+pstn = int(input('Player1, select position: '))
+if pstn == 1:
+    if board[2][0] == 'X' or board[2][0] == 'O':
+        print("Sorry, position already occupied, please choose another position")
+    else:
+        board[2][0] = player1
+    displayBoard()
